5f83f2342814c3b9d582208bfa01b8e39d95aef8
kirbyneaton <kjneat@gmail.com>
2022-03-02 00:00:04 -0500

    Fix bootstrap user error

--- frontend/product_hunt.jsx
+++ frontend/product_hunt.jsx
@@ -6,7 +6,19 @@ import * as SessionApiUtil from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    };
     ReactDOM.render(<Root store={ store} />, root);
 
     // just for testing!
