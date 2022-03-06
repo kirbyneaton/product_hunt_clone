49b9826587395f289ca9dd90934c75fa1aa9a6e3
kirbyneaton <kjneat@gmail.com>
2022-03-06 06:02:19 -0500

    Add default img to product index items

--- frontend/components/product_index/product_index_item.jsx
+++ frontend/components/product_index/product_index_item.jsx
@@ -18,14 +18,16 @@ class ProductIndexItem extends React.Component {
         return(
             <div>
                 <li className="product-item">
-                    <img src="" alt="product-picture" />
-                    <Link className="product-title">{product.title}</Link>
-                    <br />
-                    <Link className="product-description">{product.description}</Link>
-                    {/* <Link>Edit</Link>
-                    <button type="submit" onClick={this.handleSubmit}>Delete</button> */}
-                    <p className="comment-count"># of comments: 0</p>
-                    {/* number of comments? */}
+                    <img src="https://raw.githubusercontent.com/kirbyneaton/product_hunt_clone/product-index/app/assets/images/fish1.jpeg" alt="product-picture" />
+                    <div className="product-text-container">
+                        <Link className="product-title">{product.title}</Link>
+                        <br />
+                        <Link className="product-description">{product.description}</Link>
+                        {/* <Link>Edit</Link>
+                        <button type="submit" onClick={this.handleSubmit}>Delete</button> */}
+                        <p className="comment-count"># of comments: 0</p>
+                        {/* number of comments? */}
+                    </div>
                 </li>
             </div>
         )
