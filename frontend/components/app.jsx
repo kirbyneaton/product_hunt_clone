18899f8ee7217d2b3eb558343ad0216eb8f14ce8
kirbyneaton <kjneat@gmail.com>
2022-03-10 14:17:58 -0500

    Replace personal links in header and add create product button

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,13 +13,14 @@ const App = () => (
         <header>
             <Link to='/' id="header-logo"><img src="https://raw.githubusercontent.com/kirbyneaton/product_hunt_clone/product-index/app/assets/images/product-hunt-logo-orange-960.png" id="logo" alt="product-hunt-logo" /></Link>
             <nav>
-                <div className="header-link">Products</div>
-                <div className="header-link">Community</div>
-                <div className="header-link">Tools</div>
-                <div className="header-link">Jobs</div>
-                <div className="header-link">About</div>
+                <a href="https://github.com/kirbyneaton/product_hunt_clone" target="_blank" className="header-link">Github</a>
+                <a href="https://www.linkedin.com/in/kirbyneaton/" target="_blank" className="header-link">LinkedIn</a>
+                <a href="https://angel.co/u/kirby-neaton" target="_blank" className="header-link">AngelList</a>
             </nav>
-            <div className="greeting-cont"><GreetingContainer /></div>
+            <div id="login-corner">
+                <button id="create-product">Submit New Fish</button>
+                <div className="greeting-cont"><GreetingContainer /></div>
+            </div>
         </header>
         <Switch>
             {/* <Route path="/products/:productId" component={ProductShowContainer} /> */}
