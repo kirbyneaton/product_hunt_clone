b26e3072b9ce485a0b96abf176fa6f614b8357cb
kirbyneaton <kjneat@gmail.com>
2022-03-10 13:39:01 -0500

    Added empty product_form and containers

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only:[:index, :new, :create, :edit, :update, :show]
     resource :session, only:[:create, :destroy] 
-    resources :products, only: [:create, :index] do
+    resources :products, except: [:new, :edit] do
       resources :comments
     end
   end
