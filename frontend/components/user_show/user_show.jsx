cecd0b69f81e7f76d25d72b8859de507af8bef83
kirbyneaton <kjneat@gmail.com>
2022-06-15 23:59:36 -0400

    add content to user_show_container

---
+++ frontend/components/user_show/user_show.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+class UserShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   comment: ''
+    // };
+
+    // this.renderErrors = this.renderErrors.bind(this);
+  }
+
+  // componentDidMount() {
+  //   this.props.fetchProductComments(this.props.productId);
+  // }
+
+  // componentWillUnmount() {
+  //   // debugger
+  //   this.props.fetchProductComments(this.props.productId);
+  //   // this.props.removeErrors();
+  // }
+
+  // componentDidUpdate(prevProps) {
+  //   // debugger
+  //   if (prevProps.comments?.length !== this.props.comments?.length) {
+  //     this.props.fetchProductComments(this.props.productId);
+  //   }
+  // }
+
+  // renderErrors() {
+  //   debugger;
+  //   return (
+  //     <ul>
+  //       {
+  //         this.props.errors.map((err, i) => (
+  //           <li key={`error-${i}`}>
+  //             {err}
+  //           </li>
+  //         ))
+  //       }
+  //     </ul>
+  //   );
+  // }
+
+  render() {
+    // let currentProduct;
+    // for (let product of this.props.products) {
+    //   if (this.props.productId === product.id) {
+    //     currentProduct = product;
+    //   }
+    // }
+    // debugger
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+
+}
+
+export default UserShow;
