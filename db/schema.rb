4dc35557a2d94acf0cb29be8cd0eb6ede61f6e7f
kirbyneaton <kjneat@gmail.com>
2022-07-03 01:17:07 -0400

    test cropping on bear prof pics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_183934) do
+ActiveRecord::Schema.define(version: 2022_07_03_035818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_03_09_183934) do
     t.string "session_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_img"
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
